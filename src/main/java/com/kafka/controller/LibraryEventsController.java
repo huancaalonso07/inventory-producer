25093eca65fe7b95e4c976dbed559c1d35fae99c
Alonso Huanca <alonsohuanca710@gmail.com>
2026-04-13 18:40:37 -0500

    Created POST endpoint

---
+++ src/main/java/com/kafka/controller/LibraryEventsController.java
@@ -0,0 +1,27 @@
+package com.kafka.controller;
+
+import com.kafka.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LibraryEventsController {
+
+    private static final Logger log = LoggerFactory.getLogger(LibraryEventsController.class);
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+
+        log.info("libraryEvent: {} ", libraryEvent);
+        //invoke the kafka producer
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+}
