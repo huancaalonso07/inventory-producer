5e8d23f659090deeabc86468190ec6241caa1164
Alonso Huanca <alonsohuanca710@gmail.com>
2026-04-16 17:19:22 -0500

    Add kafkaTemplate configuration

--- src/main/java/com/kafka/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/controller/LibraryEventsController.java
@@ -2,8 +2,6 @@ package com.kafka.controller;
 
 import com.kafka.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class LibraryEventsController {
-
-    private static final Logger log = LoggerFactory.getLogger(LibraryEventsController.class);
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
