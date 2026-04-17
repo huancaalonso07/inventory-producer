cd849262ad11582be08bdd5d77e1edd08e912451
Alonso Huanca <alonsohuanca710@gmail.com>
2026-04-17 18:44:56 -0500

    Build LibraryEventsProducer using KafkaTemplate

---
+++ src/main/java/com/kafka/poducer/LibraryEventsProducer.java
@@ -0,0 +1,50 @@
+package com.kafka.poducer;
+
+import com.kafka.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+@Slf4j
+public class LibraryEventsProducer {
+
+    @Value("${spring.kafka.topic}")
+    public String topic;
+
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public LibraryEventsProducer(KafkaTemplate<Integer, String> kafkaTemplate, ObjectMapper objectMapper){
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvent(LibraryEvent libraryEvent){
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        var completableFuture = kafkaTemplate.send(topic, key, value);
+        return completableFuture
+                .whenComplete((sendResult, throwable) -> {
+                    if(throwable != null){
+                        handleFailure(key, value, throwable);
+                    }else {
+                        handleSuccess(key, value, sendResult);
+                    }
+                });
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
+        log.info("Message Sent Successfully for the key: {} and the value: {}, partition is {} ", key, value, sendResult.getProducerRecord().partition());
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error sending the message and the exception is {}", ex.getMessage(), ex);
+    }
+}
