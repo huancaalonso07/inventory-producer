ad413f573088b993f481066799766045fe7748a5
Alonso Huanca <alonsohuanca710@gmail.com>
2026-04-23 18:33:05 -0500

    Add different KafkaTemplate usage approaches in Producer

--- src/main/java/com/kafka/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/controller/LibraryEventsController.java
@@ -1,6 +1,7 @@
 package com.kafka.controller;
 
 import com.kafka.domain.LibraryEvent;
+import com.kafka.poducer.LibraryEventsProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,16 +9,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 @RestController
 @Slf4j
 public class LibraryEventsController {
 
+    private final LibraryEventsProducer libraryEventsProducer;
+
+    public LibraryEventsController(LibraryEventsProducer libraryEventsProducer) {
+        this.libraryEventsProducer = libraryEventsProducer;
+    }
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws ExecutionException, InterruptedException, TimeoutException {
 
         log.info("libraryEvent: {} ", libraryEvent);
         //invoke the kafka producer
+        //libraryEventsProducer.sendLibraryEvent(libraryEvent);
+        //libraryEventsProducer.sendLibraryEvent_approach2(libraryEvent);
+        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
 
+        log.info("After sending libraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
--- src/main/java/com/kafka/poducer/LibraryEventsProducer.java
+++ src/main/java/com/kafka/poducer/LibraryEventsProducer.java
@@ -2,13 +2,20 @@ package com.kafka.poducer;
 
 import com.kafka.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Component
 @Slf4j
@@ -29,7 +36,10 @@ public class LibraryEventsProducer {
         var key = libraryEvent.libraryEventId();
         var value = objectMapper.writeValueAsString(libraryEvent);
 
+        //1. Blocking call - get metadata about the kafka cluster
+        //2. Send message happens - Returns a CompletableFuture
         var completableFuture = kafkaTemplate.send(topic, key, value);
+
         return completableFuture
                 .whenComplete((sendResult, throwable) -> {
                     if(throwable != null){
@@ -40,8 +50,47 @@ public class LibraryEventsProducer {
                 });
     }
 
+    public SendResult<Integer, String> sendLibraryEvent_approach2(LibraryEvent libraryEvent) throws ExecutionException, InterruptedException, TimeoutException {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        //1. Blocking call - get metadata about the kafka cluster
+        //2. Block and wait until thr message is sent to the kafka
+        var sendResult = kafkaTemplate.send(topic, key, value)
+                //.get();
+                        .get(3, TimeUnit.SECONDS);
+        handleSuccess(key, value, sendResult);
+        return sendResult;
+    }
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvent_approach3(LibraryEvent libraryEvent){
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        var producerRecord = buildProducerRecord(key, value);
+        //1. Blocking call - get metadata about the kafka cluster
+        //2. Send message happens - Returns a CompletableFuture
+        var completableFuture = kafkaTemplate.send(producerRecord);
+
+        return completableFuture
+                .whenComplete((sendResult, throwable) -> {
+                    if(throwable != null){
+                        handleFailure(key, value, throwable);
+                    }else {
+                        handleSuccess(key, value, sendResult);
+                    }
+                });
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value) {
+
+        List<Header> recordHeaders = List.of(new RecordHeader("event-source", "scnaner".getBytes()));
+
+        return new ProducerRecord<>(topic, null, key, value, recordHeaders);
+    }
+
     private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
-        log.info("Message Sent Successfully for the key: {} and the value: {}, partition is {} ", key, value, sendResult.getProducerRecord().partition());
+        log.info("Message Sent Successfully for the key: {} and the value: {}, partition is {} ", key, value, sendResult.getRecordMetadata().partition());
     }
 
     private void handleFailure(Integer key, String value, Throwable ex) {
