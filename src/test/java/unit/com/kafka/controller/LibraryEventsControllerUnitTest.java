ebd96c08c6fc5ac766f9ee78bd4a7d148e63288c
Alonso Huanca <alonsohuanca710@gmail.com>
2026-04-28 12:41:54 -0500

    Add Unit Test to the controller

---
+++ src/test/java/unit/com/kafka/controller/LibraryEventsControllerUnitTest.java
@@ -0,0 +1,50 @@
+package com.kafka.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafka.domain.LibraryEvent;
+import com.kafka.poducer.LibraryEventsProducer;
+import com.kafka.util.TestUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(LibraryEventsController.class)
+class LibraryEventsControllerUnitTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    LibraryEventsProducer libraryEventsProducer;
+
+    @Test
+    void postLibraryEvent() throws Exception {
+
+        var json = objectMapper.writeValueAsString(TestUtil.libraryEventRecord());
+
+        when(libraryEventsProducer.sendLibraryEvent_approach3(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/v1/libraryevent")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+}
