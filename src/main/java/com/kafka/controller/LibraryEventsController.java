a9aa97512386f2e60d211c204d3751ce5c74e5d4
Alonso Huanca <alonsohuanca710@gmail.com>
2026-04-30 12:20:14 -0500

    Add udpdate endpoint on controller

--- src/main/java/com/kafka/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/controller/LibraryEventsController.java
@@ -2,12 +2,14 @@ package com.kafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.domain.LibraryEvent;
+import com.kafka.domain.LibraryEventType;
 import com.kafka.poducer.LibraryEventsProducer;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +37,30 @@ public class LibraryEventsController {
 
         log.info("After sending libraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> updateLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws ExecutionException, InterruptedException, TimeoutException, JsonProcessingException {
+
+        log.info("libraryEvent: {} ", libraryEvent);
+
+        ResponseEntity<String> BAD_REQUEST = validateLibraryEvent(libraryEvent);
+        if (BAD_REQUEST != null) return BAD_REQUEST;
+
+        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
+
+        log.info("After sending libraryEvent");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    private static ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent) {
+        if(libraryEvent.libraryEventId() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please, pass the library");
+        }
+
+        if(!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE event type supported");
+        }
+        return null;
     }
 }
